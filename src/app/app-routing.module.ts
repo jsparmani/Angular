b869c245cbb3fbb7400cebfd0b5fd7917bda42e8
Jay Parmani <jsparmani@gmail.com>
2019-08-15 11:11:52 +0530

    add angular code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
-
+import { RouterModule, Routes } from '@angular/router';
+import { CustomersListComponent } from './customers-list/customers-list.component';
+import { CreateCustomerComponent } from './create-customer/create-customer.component';
+import { SearchCustomersComponent } from './search-customers/search-customers.component';
+ 
+const routes: Routes = [
+    { path: '', redirectTo: 'customer', pathMatch: 'full' },
+    { path: 'customer', component: CustomersListComponent },
+    { path: 'add', component: CreateCustomerComponent },
+    { path: 'findbyage', component: SearchCustomersComponent },
+];
+ 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
+ 
 export class AppRoutingModule { }
